df29378b925a2fcf86c8a5be22ff428f4b4f3076
Sam Wilkins <samwilkins333@gmail.com>
2018-08-25 03:06:19 -0700

    Stripped back sms support, broadened error catching and to avoid timeouts and rendering issues, refresh the driver page multiple times

--- src/WebdriverScript/Constants.java
+++ src/WebdriverScript/Constants.java
@@ -1,23 +1,33 @@
 package WebdriverScript;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 class Constants {
-    static final char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    static final List<char[]> alphabet;
+    static {
+        alphabet = new ArrayList<>();
+        alphabet.add(new char[] {'a', 'b', 'c'});
+        alphabet.add(new char[] {'d', 'e', 'f'});
+        alphabet.add(new char[] {'g', 'h', 'i'});
+        alphabet.add(new char[] {'j', 'k', 'l'});
+        alphabet.add(new char[] {'m', 'n', 'o'});
+        alphabet.add(new char[] {'p', 'q', 'r'});
+        alphabet.add(new char[] {'s', 't', 'u'});
+        alphabet.add(new char[] {'v', 'w', 'x'});
+        alphabet.add(new char[] {'y', 'z'});
+    }
     static final Duration pause = Duration.ofSeconds(1);
     static final Duration cookieLoad = Duration.ofSeconds(7);
 
     static final String urlRoot = "http://scrabble.merriam.com/words/start-with/";
     static final String chrome = "webdriver.chrome.driver";
-    static final String fileName = "scrabbledictionaryD.txt";
+    static final String fileName = "scrabbledictionary.txt";
 
     static final String dismissCookiePath = ".//a[@aria-label='dismiss cookie message']";
     static final String driverPath = "/Users/swilkinss2012/Documents/GitHub/WebdriverScript/chromedriver";
     static final String expandPath = ".//div[contains(@class, 'sbl_word_group closed')]";
     static final String showAllPath = ".//button[contains(@class, 'sbl_load_all')]";
     static final String wordElementPath = ".//a[contains(@href, '/finder/')]";
-
-    static final String username = "samwilkins";
-    static final String password = "Jemmeister3!";
-    static final String recipient = "14158239674";
 }
--- src/WebdriverScript/CreateDictionary.java
+++ src/WebdriverScript/CreateDictionary.java
@@ -1,18 +1,12 @@
 package WebdriverScript;
 
-import com.textmagic.sms.TextMagicMessageService;
-import com.textmagic.sms.exception.ServiceBackendException;
-import com.textmagic.sms.exception.ServiceTechnicalException;
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,78 +14,66 @@ import static WebdriverScript.Constants.*;
 
 public class CreateDictionary {
 
-    private static TextMagicMessageService service;
-
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 
         try {
 
             PrintWriter writer = new PrintWriter(fileName);
-            service = new TextMagicMessageService(username, password);
 
             System.setProperty(chrome, driverPath);
             WebDriver driver = new ChromeDriver();
             Actions actions = new Actions(driver);
 
-            for (char let : alphabet) {
-                String letter = String.valueOf(let);
-                driver.get(urlRoot + letter);
+            for (char[] subset : alphabet) {
+                int i = 0;
 
-                if (letter.equals("a")) {
-                    actions.pause(cookieLoad).perform();
-                    driver.findElement(By.xpath(dismissCookiePath)).click();
+                for (char let : subset) {
+                    String letter = String.valueOf(let);
+                    driver.get(urlRoot + letter);
+
+                    i++;
+                    if (i == 1) {
+                        actions.pause(cookieLoad).perform();
+                        driver.findElement(By.xpath(dismissCookiePath)).click();
+                    }
+
+                    System.out.printf("\n*** %s ***\n\n", letter.toUpperCase());
+                    List<WebElement> categories = driver.findElements(By.xpath(expandPath));
+                    System.out.printf("Page has %s categories to expand.\n", categories.size());
+                    categories.forEach(webElement -> {
+                        actions.moveToElement(webElement).pause(pause).perform();
+                        webElement.click();
+                        actions.pause(pause).perform();
+                    });
+
+                    List<WebElement> showAll = driver.findElements(By.xpath(showAllPath));
+                    System.out.printf("Page has %s 'show all' buttons.\n", showAll.size());
+                    showAll.forEach(webElement -> {
+                        actions.moveToElement(webElement).pause(pause).perform();
+                        webElement.click();
+                        actions.pause(pause).perform();
+                    });
+
+                    List<String> newWords = driver.findElements(By.xpath(wordElementPath)).stream().map(WebElement::getText).filter(s -> !s.equals("")).collect(Collectors.toList());
+                    System.out.printf("Letter %s had %s non-empty words.\n", letter.toUpperCase(), newWords.size());
+
+                    newWords.forEach(writer::println);
                 }
 
-                System.out.printf("\n*** %s ***\n\n", letter.toUpperCase());
-                List<WebElement> categories = driver.findElements(By.xpath(expandPath));
-                System.out.printf("Page has %s categories to expand.\n", categories.size());
-                categories.forEach(webElement -> {
-                    actions.moveToElement(webElement).pause(pause).perform();
-                    webElement.click();
-                    actions.pause(pause).perform();
-                });
-
-                List<WebElement> showAll = driver.findElements(By.xpath(showAllPath));
-                System.out.printf("Page has %s 'show all' buttons.\n", showAll.size());
-                showAll.forEach(webElement -> {
-                    actions.moveToElement(webElement).pause(pause).perform();
-                    webElement.click();
-                    actions.pause(pause).perform();
-                });
-
-                List<String> newWords = driver.findElements(By.xpath(wordElementPath)).stream().map(WebElement::getText).filter(s -> !s.equals("")).collect(Collectors.toList());
-                System.out.printf("Letter %s had %s non-empty words.\n", letter.toUpperCase(), newWords.size());
-
-                newWords.forEach(writer::println);
-
-                String message = String.format("Letter %s had %s non-empty words.\n", letter.toUpperCase(), newWords.size());
-                service.send(message, recipient);
+                driver.close();
             }
 
             writer.close();
 
             long elapsedTime = (System.currentTimeMillis() - startTime) / 1000;
-            String message = String.format("Dictionary extraction complete! Process took %s seconds!", elapsedTime);
-            service.send(message, recipient);
+            System.out.printf("Dictionary extraction complete! Process took %s seconds!\n", elapsedTime);
 
-        } catch (FileNotFoundException | ServiceBackendException | ServiceTechnicalException e) {
+        } catch (Exception e) {
 
+            long elapsedTime = (System.currentTimeMillis() - startTime) / 1000;
+            System.out.printf("UH OH! Exception hit after %s seconds!\n\n", elapsedTime);
             e.printStackTrace();
-
-        } catch (TimeoutException t) {
-
-            try {
-
-                long elapsedTime = (System.currentTimeMillis() - startTime) / 1000;
-                String message = String.format("*** Oh no! Script timed out after %s seconds ***", elapsedTime);
-                service.send(message, recipient);
-
-            } catch (ServiceBackendException | ServiceTechnicalException e) {
-
-                e.printStackTrace();
-
-            }
 
         }
     }
