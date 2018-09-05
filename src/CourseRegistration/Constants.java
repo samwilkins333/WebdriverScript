dbc76c963b15eed3d999306dd661766bbe837df8
Sam Wilkins <samwilkins333@gmail.com>
2018-09-04 22:02:12 -0400

    Registration

---
+++ src/CourseRegistration/Constants.java
@@ -0,0 +1,8 @@
+package CourseRegistration;
+
+class Constants {
+
+    static final String chrome = "webdriver.chrome.driver";
+    static final String driverPath = "/Users/swilkinss2012/Documents/GitHub/WebdriverScript/chromedriver";
+
+}
